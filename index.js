9f6673919e78aa2870547b99848b3b272b74f0ad
bforbit <115847484+bforbit@users.noreply.github.com>
2025-05-04 11:30:36 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ btnGetScheme.addEventListener("click", function(){
    let colorType = document.getElementById("color-type").value
    console.log(seedColor)
    document.getElementById("color-hex-display-test").innerHTML = seedColor + " and " + colorType
+
 })
+
+   fetch ("https://www.thecolorapi.com")
+   .then(res=>res.json())
+   .then(data=>{
+      const colorsSection = data.slice(0,5)
+      let colorsSet = ""
+      for (let color of colorsSection){
+         colorsSet += 
+         <div class="color-block">
+            <div class="color"></div>
+            <div class="hex">${color.hex.value}</div>
+         </div>
+      }
+      document.querySelector(".five-color-section").innerHTML = colorsSet
+})
+
