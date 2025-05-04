c0b4bca5a0248d79994b2118d63db249784f1cf1
bforbit <115847484+bforbit@users.noreply.github.com>
2025-05-04 19:09:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const btnGetScheme = document.getElementById("get-scheme-btn")
-const colorBlocks = document.querySelectorAll(".color-block")
-const hexCodes = document.querySelectorAll(".hex")
 
 btnGetScheme.addEventListener("click", function(){
-   let seedColor = document.getElementById("seed-color").value
-   let colorType = document.getElementById("color-type").value
-   console.log(seedColor)
-   document.getElementById("color-hex-display-test").innerHTML = seedColor + " and " + colorType
-
-})
-
    fetch ("https://www.thecolorapi.com")
    .then(res=>res.json())
    .then(data=>{
       const colorsSection = data.slice(0,5)
       let colorsSet = ""
       for (let color of colorsSection){
-         colorsSet += 
+         colorsSet += `
          <div class="color-block">
             <div class="color"></div>
             <div class="hex">${color.hex.value}</div>
          </div>
+         `
       }
       document.querySelector(".five-color-section").innerHTML = colorsSet
 })
+})
 
+
+   // let seedColor = document.getElementById("seed-color").value
+   // let colorType = document.getElementById("color-type").value
+   // console.log(seedColor)
+   // document.getElementById("color-hex-display-test").innerHTML = seedColor + " and " + colorType
