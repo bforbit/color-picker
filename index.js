f3bfe036e38bc3567c6df6c17c15e10600d77dc7
bforbit <115847484+bforbit@users.noreply.github.com>
2025-05-06 12:24:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
+// Establishing color scheme button
 const btnGetScheme = document.getElementById("get-scheme-btn")
 
+// Button protocol
 btnGetScheme.addEventListener("click", function(){
-   fetch ("https://www.thecolorapi.com")
+   let seedColor = document.getElementById("seed-color").value
+   let colorType = document.getElementById("color-type").value
+
+   // remove numeric symbol from hex code
+   seedColor = seedColor.slice(1)
+
+   // connect to the API with selected hex and mode
+   fetch (`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorType}`)
    .then(res=>res.json())
    .then(data=>{
-      const colorsSection = data.slice(0,5)
+      // extract first 5 colors
+      const colorsSection = data.colors.slice(0,5)
       let colorsSet = ""
+      // use empty string to create a div with visual and hex value
       for (let color of colorsSection){
          colorsSet += `
          <div class="color-block">
-            <div class="color"></div>
+            <div class="color" style="background-color: ${color.hex.value}"></div>
             <div class="hex">${color.hex.value}</div>
          </div>
          `
       }
-      document.querySelector(".five-color-section").innerHTML = colorsSet
+   document.querySelector(".five-color-section").innerHTML = colorsSet
 })
 })
-
-
-   // let seedColor = document.getElementById("seed-color").value
-   // let colorType = document.getElementById("color-type").value
-   // console.log(seedColor)
-   // document.getElementById("color-hex-display-test").innerHTML = seedColor + " and " + colorType
